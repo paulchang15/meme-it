0ba5b9c0fb618be46e9af67c460fa3f59b3f0c50
Paul Chang <paulchang959@gmail.com>
2021-03-03 17:45:46 -0800

    fixed dashboard posts

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -9,7 +9,7 @@ router.get("/", withAuth, async (req, res) => {
     // console.log(req.body.user_id);
     const allPosts = await Post.findAll({
       where: {
-        user_id: req.session.id,
+        user_id: req.session.user_id,
       },
       attributes: [
         "id",
@@ -44,17 +44,17 @@ router.get("/", withAuth, async (req, res) => {
         { model: Image, attributes: ["img_url"] },
       ],
     });
-    console.log("--------------------------------------");
-    console.log(allPosts);
+    // console.log("--------------------------------------");
+    // console.log(allPosts);
     const posts = await allPosts.map((post) => post.get({ plain: true }));
 
     if (!allPosts) {
       res.status(404).json({ message: "No posts found!" });
       return;
     }
-    console.log("--------------------------------------");
+    console.log("-----------------dashboard posts---------------------");
     console.log(posts);
-    await res.render("dashboard", { posts, loggedIn: true });
+    await res.render("dashboard", { posts, loggedIn: req.session.loggedIn });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
