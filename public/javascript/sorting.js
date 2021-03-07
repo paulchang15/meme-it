74df41a3d2f5e75b6bf205f00991c14d680a1ea5
Paul Chang <paulchang959@gmail.com>
2021-03-06 17:15:02 -0800

    changed radio buttons to drop down

--- public/javascript/sorting.js
+++ public/javascript/sorting.js
@@ -1,23 +1,26 @@
-async function postSortHandler(event) {
-  event.preventDefault();
-  console.log("============clicked===========");
-  const sortBy = event.target.getAttribute("data-sort");
-  console.log(sortBy);
-  const sort = await fetch("/api/posts/", {
-    method: "GET",
-    params: JSON.stringify({
-      // pass in data from string to either be "default" or "vote"
-      sort: sortBy,
-    }),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-}
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll("select");
+  var instances = M.FormSelect.init(elems);
+  const selectEl = document.querySelector("#sort");
 
-document
-  .getElementById("newest-post")
-  .addEventListener("click", postSortHandler);
-document
-  .getElementById("vote-count")
-  .addEventListener("click", postSortHandler);
+  async function postSortHandler(event) {
+    event.preventDefault();
+    console.log(selectEl);
+
+    const sort = await fetch("/api/posts/", {
+      method: "GET",
+      params: JSON.stringify({
+        // pass in data from string to either be "default" or "vote"
+        sort: selectEl,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  selectEl.addEventListener("change", postSortHandler);
+});
+// document
+//   .querySelector("#sort-option")
+//   .addEventListener("click", postSortHandler);
