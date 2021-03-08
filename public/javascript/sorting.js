b84ed39bbf7e76e668bc0ec385c5d8ca17a651f3
Paul Chang <paulchang959@gmail.com>
2021-03-07 22:47:47 -0800

    reviewing edits

--- public/javascript/sorting.js
+++ public/javascript/sorting.js
@@ -1,30 +1,30 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var elems = document.querySelectorAll("select");
-  var instances = M.FormSelect.init(elems);
-  const selectEl = document.querySelector("#sort");
+// document.addEventListener("DOMContentLoaded", function () {
+//   var elems = document.querySelectorAll("select");
+//   var instances = M.FormSelect.init(elems);
+//   const selectEl = document.querySelector("#sort");
 
-  async function postSortHandler(event) {
-    // event.preventDefault();
-    console.log(selectEl);
+//   async function postSortHandler(event) {
+//     // event.preventDefault();
+//     console.log(selectEl);
 
-    const sort = await fetch(`/api/posts/?sort=${selectEl.value}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+//     const sort = await fetch(`/api/posts/?sort=${selectEl.value}`, {
+//       method: "GET",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     });
 
-    if (sort.ok) {
-      console.log("=========SORT WORKED===========");
-      // document.location.replace("/");
-      document.location.reload();
-    } else {
-      alert(sort.statusText);
-    }
-  }
+//     if (sort.ok) {
+//       console.log("=========SORT WORKED===========");
+//       // document.location.replace("/");
+//       document.location.reload();
+//     } else {
+//       alert(sort.statusText);
+//     }
+//   }
 
-  selectEl.addEventListener("change", postSortHandler);
-});
-// document
-//   .querySelector("#sort-option")
-//   .addEventListener("click", postSortHandler);
+//   selectEl.addEventListener("change", postSortHandler);
+// });
+// // document
+// //   .querySelector("#sort-option")
+// //   .addEventListener("click", postSortHandler);
--- utils/imgur.js
+++ utils/imgur.js
@@ -1,63 +1,63 @@
-var imgur = require("imgur");
-let image = "";
+// var imgur = require("imgur");
+// let image = "";
 
-// add this to public javascript file
-imgur.setClientId("ebe2f73bc0d1a0d");
+// // add this to public javascript file
+// imgur.setClientId("ebe2f73bc0d1a0d");
 
-imgur.getClientId();
-//
+// imgur.getClientId();
+// //
 
-async function saveId() {
-  try {
-    await imgur.saveClientId(path);
-    await function () {
-      console.log("Saved.");
-    };
-  } catch (err) {
-    console.error(err.message);
-  }
-}
+// async function saveId() {
+//   try {
+//     await imgur.saveClientId(path);
+//     await function () {
+//       console.log("Saved.");
+//     };
+//   } catch (err) {
+//     console.error(err.message);
+//   }
+// }
 
-async function loadId() {
-  try {
-    await imgur.loadClientId(path);
-    await imgur.setClientId();
-  } catch (err) {
-    console.error(err.message);
-  }
-}
+// async function loadId() {
+//   try {
+//     await imgur.loadClientId(path);
+//     await imgur.setClientId();
+//   } catch (err) {
+//     console.error(err.message);
+//   }
+// }
 
-async function fileUpload(image) {
-  try {
-    const upload = await imgur.uploadFile(image);
+// async function fileUpload(image) {
+//   try {
+//     const upload = await imgur.uploadFile(image);
 
-    return upload.data.link;
-  } catch (err) {
-    console.error(err.message);
-  }
-}
+//     return upload.data.link;
+//   } catch (err) {
+//     console.error(err.message);
+//   }
+// }
 
-async function urlUpload(image) {
-  try {
-    const urlUpload = await imgur.uploadUrl(image);
+// async function urlUpload(image) {
+//   try {
+//     const urlUpload = await imgur.uploadUrl(image);
 
-    return urlUpload.data.link;
-  } catch (err) {
-    console.error(err.message);
-  }
-}
+//     return urlUpload.data.link;
+//   } catch (err) {
+//     console.error(err.message);
+//   }
+// }
 
-//route to get image from
-async function getImage(imageId) {
-  try {
-    const image = await imgur.getInfo(imageId);
-    console.log(image);
-  } catch (err) {
-    console.log(err);
-    return err;
-  }
-}
+// //route to get image from
+// async function getImage(imageId) {
+//   try {
+//     const image = await imgur.getInfo(imageId);
+//     console.log(image);
+//   } catch (err) {
+//     console.log(err);
+//     return err;
+//   }
+// }
 
-async function deleteImage(image) {}
+// async function deleteImage(image) {}
 
-module.exports = { saveId, loadId, fileUpload, urlUpload };
+// module.exports = { saveId, loadId, fileUpload, urlUpload };
